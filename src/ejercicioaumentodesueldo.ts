6386ee787660ce307cab4b59e231f883095139f3
Oriana López <104174216+Oriana-Lopez@users.noreply.github.com>
2022-05-04 19:42:43 -0300

    ejercicioaumentodesueldo

--- src/ejercicioaumentodesueldo.ts
+++ src/ejercicioaumentodesueldo.ts
@@ -2,18 +2,17 @@ let sueldoActual : number = Number(prompt("Ingrese el salario actual"));
 let nuevoSueldo : number = 0;
 
 switch sueldoActual{
-  
-  case 15000:
+  case 10000:
       if ( 0 < sueldoActual && sueldoActual <= 15000 ) 
       nuevoSueldo = sueldoActual*1.2;
       console.log("Aumento de 20%");
       break;
-  case 20000:
+  case 18000:
       if ( 15001 < sueldoActual && sueldoActual <= 20000 ) 
       nuevoSueldo = sueldoActual*1.1;
       console.log("Aumento de 10%");
       break;
-  case 25000:
+  case 23400:
       if ( 20001 < sueldoActual && sueldoActual <= 25000 ) 
       nuevoSueldo = sueldoActual * 1.05;
       console.log("Aumento de 5%");
